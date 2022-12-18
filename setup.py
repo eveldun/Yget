c3a0b12b595fce5cbd82d9869a7231c45cd5ccfe
eveldun <eveldun123@gmail.com>
2022-12-17 23:30:29 -0800

    Started yget

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='yget',
+    version='0.9',
+    packages=[''],
+    url='https://github.com/eveldun/Yget',
+    license='https://github.com/eveldun/Yget/blob/main/LICENSE',
+    author='eveldun',
+    author_email='eveldun785@gmail.com',
+    description='Yget - The open source youtube downloading tool'
+)
