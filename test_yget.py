882c0777214c2a3e9b6331343ac9f050fcceed16
eveldun <eveldun123@gmail.com>
2022-12-20 15:20:32 -0800

    Test Files for building

---
+++ test_yget.py
@@ -0,0 +1,18 @@
+from unittest import TestCase
+
+
+class Test(TestCase):
+    def test_completed(self):
+        self.fail()
+
+    def test_show_progress_bar(self):
+        self.fail()
+
+    def test_yt_convert(self):
+        self.fail()
+
+    def test_process(self):
+        self.fail()
+
+    def test_bcolors(self):
+        self.fail()
