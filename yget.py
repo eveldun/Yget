900f42ba787e5ec7e874465f65948183a805ec9d
eveldun <eveldun123@gmail.com>
2022-12-21 01:36:19 -0800

    Test Files for building

--- yget.py
+++ yget.py
@@ -13,8 +13,7 @@ import pytube
 Varg = 1
 arguments = argparse.ArgumentParser(prog="yget", description="Converts Your Favorite Songs To Downloaded Format")
 arguments.add_argument("VideoUrl", type=str, help="The Video Url")
-arguments.add_argument("-f", "--File-format", action="store_const", const=Varg, dest="Data", default="mp4",
-                       help="Video File Format: Mp3,Mp4 Default:Mp4")
+arguments.add_argument("-f", "--File-format",dest="Data", default="mp4", help="Video File Format: Mp3,Mp4 Default:Mp4")
 
 
 class bcolors:
